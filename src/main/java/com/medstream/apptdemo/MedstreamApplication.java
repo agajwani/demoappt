89ad829171ef8bc368e86ae09082bf4cb5fa3146
Grant Gustin <GrantGustin@MEDSTREAM.local>
2017-11-07 12:48:45 +0530

    Initial commit

---
+++ src/main/java/com/medstream/apptdemo/MedstreamApplication.java
@@ -0,0 +1,12 @@
+package com.medstream.apptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedstreamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/medstream/apptdemo/MedstreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medstream.apptdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedstreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
